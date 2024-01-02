c49157e2c2197db4516a229e10a8ba130c150be0
Seno Dwi Prasetyo <senothoha77@gmail.com>
2024-01-02 11:05:03 +0700

    layout

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Home : The best athlete wants his opponent at his best.
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', \App\Livewire\Home::class)->name('home');
+
+Route::get('/about', \App\Livewire\About::class)->name('about');
